a619f79e9d8c75097a27f1ea6c0a3d124ab6de9b
Bartek <bgrzegorczyk95@gmail.com>
2020-07-02 16:06:14 +0200

    Updates

---
+++ precache-manifest.db34030ebc4d5a13f33f558b4178be20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab0a4d382ad3cfaae5055031678ff6d",
+    "url": "/faktura/index.html"
+  },
+  {
+    "revision": "b6a64ebbc564b5c60eec",
+    "url": "/faktura/static/js/2.cf23dbbb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/faktura/static/js/2.cf23dbbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9895dc32f8f6a4ff3780",
+    "url": "/faktura/static/js/main.8741731d.chunk.js"
+  },
+  {
+    "revision": "208a95b05712deb9b605",
+    "url": "/faktura/static/js/runtime-main.9e7fb27e.js"
+  }
+]);
